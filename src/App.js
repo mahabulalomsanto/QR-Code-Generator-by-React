f56515b3a4af70e74aa8afa7dc672882d8d8b1f5
Mahabul Alom Santo <santo@Mahabuls-MacBook-Air.local>
2025-05-25 21:39:52 +0600

    QR Generator Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { QRCodeSVG } from "qrcode.react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [Text, setText] = useState("");
+	const [showQR, setShowQR] = useState(false);
+	const [showInput, setShowInput] = useState(true);
+	
+  const QrGenerate = () => {
+		if (Text) {
+			setShowQR(true);
+			setShowInput(false);
+		} else {
+			alert("Please enter some text to generate QR Code");
+			setShowQR(false);
+		}
+	};
+
+	return (
+		<div className="App">
+			<h1>QR Code Generator</h1>
+			{showInput && (
+				<div className="input-area">
+					<input
+						type="text"
+						value={Text}
+						onChange={(e) => {
+							setText(e.target.value);
+							setShowQR(false);
+						}}></input>
+					<button onClick={QrGenerate}>Generate QR Code</button>
+				</div>
+			)}
+
+			<div className="qr-area">
+				{showQR && 
+					<div>
+						<QRCodeSVG value={Text} size={256}></QRCodeSVG>
+						<button onClick={()=>
+              {
+                setShowInput(true);
+                setShowQR(false);
+                setText("");
+              }}>Generate Another One</button>
+					</div>
+				}
+			</div>
+		</div>
+	);
 }
 
 export default App;
